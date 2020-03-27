f7aab1967658f97eb175ca1deb84104a81e2c9dd
Rohan Agrawal <send2arohan@gmail.com>
2020-03-27 13:13:43 -0700

    Split twist sending into continous vs single modes
    Some control methods (like joystick) have the repeating
    built in, so they don't need continous sending.

--- public/main.js
+++ public/main.js
@@ -4,7 +4,7 @@ class Twist {
 		this.continious_sending = false;
 		this.linear = 0.0;
 		this.angular = 0.0;
-		window.setInterval(this.continious_send.bind(this), 100);
+		window.setInterval(this.send_loop.bind(this), 100);
 
 		this.cmdVel = new ROSLIB.Topic({
 			ros : ros,
@@ -12,6 +12,26 @@ class Twist {
 			messageType : 'geometry_msgs/Twist'
 		});
 
+	}
+
+	send = (linear, angular) => {
+		this.linear = linear;
+		this.angular = angular;
+		this.continious_sending = false;
+		this.twist(linear,angular);
+	}
+
+	continious_send = (linear, angular) => {
+		this.linear = linear;
+		this.angular = angular;
+		this.continious_sending = true;
+		this.twist(linear,angular);
+	}
+
+	clear = () => {
+		this.continious_sending = false;
+		this.linear = this.angular = 0.0;
+		this.twist(0, 0);
 	}
 
 	twist = (forward, rotate) => {
@@ -32,23 +52,7 @@ class Twist {
 		this.cmdVel.publish(twist);
 	}
 
-	clear = () => {
-		if(!this.continious_sending) return;
-
-		this.continious_sending = false;
-		this.linear = this.angular = 0.0;
-		this.twist(0, 0);
-	}
-
-
-	send = (linear, angular) => {
-		this.linear = linear;
-		this.angular = angular;
-		this.continious_sending = true;
-		this.twist(linear,angular);
-	}
-
-	continious_send = () => {
+	send_loop = () => {
 		if(this.continious_sending) {
 			this.twist(this.linear,this.angular);
 		}
@@ -63,10 +67,10 @@ window.addEventListener('load', function() {
 	var twist = new Twist('/cmd_vel');
 
 	var landscape_input = new Landscape();
-	landscape_input.bind_sliders(twist.send, twist.clear);
+	landscape_input.bind_sliders(twist.continious_send, twist.clear);
 
 	var portrait_input = new Portrait();
-	portrait_input.bind_buttons(twist.send, twist.clear);
+	portrait_input.bind_buttons(twist.continious_send, twist.clear);
 	portrait_input.bind_joystick(twist.send, twist.clear);
 
 	var battery_state = new BatteryState('/battery_state');
